d9b363c2c8cdf53afcd101b31ebabe4315fa3a48
Pasqualino Sorice <lino.sorice@gmail.com>
2018-10-04 08:26:38 +0200

    add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": [
+        "airbnb-base",
+        "prettier"
+    ],
+    "env": {
+        "jest": true
+    }
+};
--- test/formating.test.js
+++ test/formating.test.js
@@ -1,23 +1,23 @@
 const Zemen = require('../zemen');
 const Formatter = require('../lib/formating');
 
-let zare = new Zemen('2009-12-27');
-let day2 = new Zemen('2009-1-1');
+const zare = new Zemen('2009-12-27');
+const day2 = new Zemen('2009-1-1');
 
 describe('Zemen Formating', () => {
     describe('Default & ISO8601', () => {
         (function () {
-            let expected = '2009-12-27';
+            const expected = '2009-12-27';
             it(`Default Formating should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format();
+                const actual = zare.format();
                 expect(actual).toEqual(expected);
             });
 
         })();
         (function () {
-            let expected = '2009-12-27';
+            const expected = '2009-12-27';
             it(`ISO8601 Formating should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format();
+                const actual = zare.format();
                 expect(actual).toEqual(expected);
             });
         })();
@@ -25,50 +25,50 @@ describe('Zemen Formating', () => {
 
     describe('Year Patterns', () => {
         (function () {
-            let pattern = 'Y';
-            let expected = '2009';
+            const pattern = 'Y';
+            const expected = '2009';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'YY';
-            let expected = '09';
+            const pattern = 'YY';
+            const expected = '09';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'YYY';
-            let expected = '092009';
+            const pattern = 'YYY';
+            const expected = '092009';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'YYYY';
-            let expected = '2009';
+            const pattern = 'YYYY';
+            const expected = '2009';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'YYYYY';
-            let expected = '20092009';
+            const pattern = 'YYYYY';
+            const expected = '20092009';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'YY';
-            let expected = '09';
+            const pattern = 'YY';
+            const expected = '09';
             it(`${pattern} should format ${day2.toString()} as ${expected}`, () => {
-                let actual = day2.format(pattern);
+                const actual = day2.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
@@ -76,50 +76,50 @@ describe('Zemen Formating', () => {
 
     describe('Month Patterns', () => {
         (function () {
-            let pattern = 'M';
-            let expected = '12';
+            const pattern = 'M';
+            const expected = '12';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'MM';
-            let expected = '12';
+            const pattern = 'MM';
+            const expected = '12';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'MMM';
-            let expected = 'ነሐሴ';
+            const pattern = 'MMM';
+            const expected = 'ነሐሴ';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'MMMM';
-            let expected = 'ነሐሴ';
+            const pattern = 'MMMM';
+            const expected = 'ነሐሴ';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'MMMMM';
-            let expected = 'ነሐሴ12';
+            const pattern = 'MMMMM';
+            const expected = 'ነሐሴ12';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'MM';
-            let expected = '01';
+            const pattern = 'MM';
+            const expected = '01';
             it(`${pattern} should format ${day2.toString()} as ${expected}`, () => {
-                let actual = day2.format(pattern);
+                const actual = day2.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
@@ -127,50 +127,50 @@ describe('Zemen Formating', () => {
 
     describe('Day Patterns', () => {
         (function () {
-            let pattern = 'D';
-            let expected = '27';
+            const pattern = 'D';
+            const expected = '27';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'DD';
-            let expected = '27';
+            const pattern = 'DD';
+            const expected = '27';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'DDD';
-            let expected = 'ቅዳሜ';
+            const pattern = 'DDD';
+            const expected = 'ቅዳሜ';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'd';
-            let expected = 'ቅዳሜ';
+            const pattern = 'd';
+            const expected = 'ቅዳሜ';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'DDDDD';
-            let expected = 'ቅዳሜ27';
+            const pattern = 'DDDDD';
+            const expected = 'ቅዳሜ27';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'DD';
-            let expected = '01';
+            const pattern = 'DD';
+            const expected = '01';
             it(`${pattern} should format ${day2.toString()} as ${expected}`, () => {
-                let actual = day2.format(pattern);
+                const actual = day2.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
@@ -178,26 +178,26 @@ describe('Zemen Formating', () => {
 
     describe('Mixed', () => {
         (function () {
-            let pattern = 'd MMM/DD/YYYY';
-            let expected = 'ቅዳሜ ነሐሴ/27/2009';
+            const pattern = 'd MMM/DD/YYYY';
+            const expected = 'ቅዳሜ ነሐሴ/27/2009';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'd ፣ MMM DD ቀን YYYY E';
-            let expected = 'ቅዳሜ ፣ ነሐሴ 27 ቀን 2009 ዓ.ም';
+            const pattern = 'd ፣ MMM DD ቀን YYYY E';
+            const expected = 'ቅዳሜ ፣ ነሐሴ 27 ቀን 2009 ዓ.ም';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
         (function () {
-            let pattern = 'e';
-            let expected = '6';
+            const pattern = 'e';
+            const expected = '6';
             it(`${pattern} should format ${zare.toString()} as ${expected}`, () => {
-                let actual = zare.format(pattern);
+                const actual = zare.format(pattern);
                 expect(actual).toEqual(expected);
             });
         })();
@@ -205,7 +205,7 @@ describe('Zemen Formating', () => {
 
     describe('Exceptions', () => {
         it(`Formatter should return empty string if date is  undefined `, () => {
-            let actual = Formatter.format(null);
+            const actual = Formatter.format(null);
             expect(actual).toBe("");
         });
     })
